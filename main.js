3162a422e71c1e6c953585ccabea4ebd0b85cd9a
Illia Dubrovin <75540413+aliveGUY@users.noreply.github.com>
2022-04-01 02:26:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 
 //import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
